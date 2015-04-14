dc69b0f9809a3b198e7620a5f43d11ba3b6834a6
Scott Bellware <sbellware@gmail.com>
2015-04-14 01:10:02 -0500

    Library dependencies are loaded by the library's loader

--- init.rb
+++ init.rb
@@ -5,9 +5,3 @@ lib_dir = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift lib_dir unless $LOAD_PATH.include?(lib_dir)
 
 require 'data_access/postgresql/connector'
-require 'clock'
-require 'attribute'
-require 'null_attr'
-require 'dependency'
-require 'logger'
-require 'settings'
--- lib/data_access/postgresql/connector.rb
+++ lib/data_access/postgresql/connector.rb
@@ -1,4 +1,8 @@
 require 'sequel'
 require 'jdbc/postgres'
 
+require 'dependency'
+require 'logger'
+require 'settings'
+
 require 'data_access/postgresql/connector/connector'
