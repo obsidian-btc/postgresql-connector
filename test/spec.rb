0c5e0ff585e0475df9dc45d8f4729f92bf45bf38
Scott Bellware <sbellware@gmail.com>
2015-04-14 02:07:06 -0500

    Test utility files are excluded from the suite using the runner's exclude block

--- test/spec.rb
+++ test/spec.rb
@@ -1,8 +1,5 @@
 require_relative 'test_init'
 
-base_dir = File.expand_path(File.dirname(__FILE__))
-pattern = File.join base_dir, 'spec/postgresql_connector/*.rb'
-
-files = Dir.glob(pattern).reject { |file| file =~ /_init.rb\z/ }
-
-Runner.! files
+Runner.! 'spec/postgresql_connector/*.rb' do |exclude|
+  exclude =~ /_init.rb\z/
+end
