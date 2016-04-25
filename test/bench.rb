580a9c00ff5f6dc31821ad12698c6b3cf8555e39
Nathan Ladd <nathan@obsidianexchange.com>
2016-04-25 16:15:06 -0500

    Rename extest ⇒ exit

--- test/bench.rb
+++ test/bench.rb
@@ -5,4 +5,4 @@ system "test/setup/create_database" or fail "Could not set up database"
 TestBench::Runner.(
   'bench/**/*.rb',
   exclude_pattern: %r{/skip\.|(?:_init\.rb|\.sketch\.rb|_sketch\.rb|\.skip\.rb)\z|_integration}
-) or extest 1
+) or exit 1
